72798ac9824f1a838d7c3b4ae2210e54a3144131
arafatul-islam <marafatbs@gmail.com>
2023-05-20 15:18:06 +0600

    queue with object implementation

--- stackAndQueue/queue.js
+++ stackAndQueue/queue.js
@@ -25,4 +25,35 @@
 // console.log(queue.deleteElem());
 // console.log(queue.lookup());
 
-// queue object implementation 
+// queue object implementation
+
+class QueueObj {
+  constructor() {
+    this.obj = {};
+    this.count = 0;
+  }
+
+  addElem(val) {
+    this.obj[this.count++] = val;
+    return this.obj;
+  }
+
+  deleteElem() {
+    for (let i = 0; i < this.count; i++) {
+      this.obj[i] = this.obj[i + 1];
+    }
+    this.count--;
+    delete this.obj[this.count];
+    return this.obj;
+}  }
+
+
+const queueObj = new QueueObj();
+console.log(queueObj.addElem(1));
+console.log(queueObj.addElem(2));
+console.log(queueObj.addElem(3));
+console.log(queueObj.addElem(4));
+console.log(queueObj.addElem(5));
+console.log(queueObj.deleteElem());
+console.log(queueObj.deleteElem());
+console.log(queueObj.deleteElem());
